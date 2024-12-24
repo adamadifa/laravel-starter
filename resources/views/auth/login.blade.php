1e4b06525f34ca76406003d34aa9ae6c36bbd29e
adam adifam <adamabdi.al.a@gmail.com>
2024-12-25 05:54:21 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 
-<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
-    data-assets-path="{{ asset('/assets/') }}" data-template="vertical-menu-template-no-customizer">
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('/assets/') }}"
+    data-template="vertical-menu-template-no-customizer">
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Login Basic - Pages | Vuexy - Bootstrap Admin Template</title>
 
@@ -59,18 +58,18 @@
                 <div class="card-body">
                     <!-- Logo -->
                     <div class="app-brand justify-content-center mb-4 mt-2">
-                        <img src="{{ asset('assets/img/logo/hibah.png') }}" alt="" width="160">
+                        {{-- <img src="{{ asset('assets/img/logo/hibah.png') }}" alt="" width="160"> --}}
                     </div>
                     <!-- /Logo -->
-                    <h4 class="mb-1 pt-2">E-HIBAH! 👋</h4>
+                    <h4 class="mb-1 pt-2">SIPREN! 👋</h4>
                     <p class="mb-4">Silahkan Login !</p>
                     <x-alert-error :messages="$errors->get('id_user')" class="mt-2" />
                     <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
                         @csrf
                         <div class="mb-3">
                             <label for="id_user" class="form-label">Email or Username</label>
-                            <input type="text" class="form-control" id="id_user" name="id_user"
-                                placeholder="Enter your email or username" autofocus />
+                            <input type="text" class="form-control" id="id_user" name="id_user" placeholder="Enter your email or username"
+                                autofocus />
                         </div>
                         <div class="mb-3 form-password-toggle">
                             <div class="d-flex justify-content-between">
@@ -81,8 +80,8 @@
                             </div>
                             <div class="input-group input-group-merge">
                                 <input type="password" id="password" class="form-control" name="password"
-                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                    aria-describedby="password" autocomplete="current-password" />
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password"
+                                    autocomplete="current-password" />
                                 <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                             </div>
                         </div>
