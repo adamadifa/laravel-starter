95e77577e062d0d3cf127a719e622af7421f56c4
adam adifam <adamabdi.al.a@gmail.com>
2025-01-08 07:11:48 +0700

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -27,55 +27,56 @@
         </div>
 
     </div> --}}
-
-    <div class="row">
-        <div class="col-xl-12 col-sm-12">
-            <div class="card h-100">
-                <div class="card-header">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-0 text-body">Saldo</p>
-                        <p class="card-text fw-medium text-success"></p>
-                    </div>
-                    <h4 class="card-title mb-1">Rp. 1.000.000.000</h4>
-                </div>
-                <div class="card-body">
-                    <div class="row mb-2">
-                        <div class="col-4">
-                            <div class="d-flex gap-2 align-items-center mb-2">
-                                <span class="badge bg-label-info p-1 rounded"><i class="ti ti-shopping-cart ti-sm"></i></span>
-                                <p class="mb-0">Pemasukan</p>
-                            </div>
-                            {{-- <h5 class="mb-0 pt-1 mb-1">62.2%</h5> --}}
-                            <small class="text-muted">5.000.000</small>
+    @hasanyrole(['super admin', 'pimpinan pesantren'])
+        <div class="row">
+            <div class="col-xl-12 col-sm-12">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <p class="mb-0 text-body">Saldo</p>
+                            <p class="card-text fw-medium text-success"></p>
                         </div>
-                        <div class="col-4">
-                            <div class="divider divider-vertical">
-                                <div class="divider-text">
-                                    <span class="badge-divider-bg bg-label-secondary">VS</span>
+                        <h4 class="card-title mb-1">Rp. 1.000.000.000</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row mb-2">
+                            <div class="col-4">
+                                <div class="d-flex gap-2 align-items-center mb-2">
+                                    <span class="badge bg-label-info p-1 rounded"><i class="ti ti-shopping-cart ti-sm"></i></span>
+                                    <p class="mb-0">Pemasukan</p>
+                                </div>
+                                {{-- <h5 class="mb-0 pt-1 mb-1">62.2%</h5> --}}
+                                <small class="text-muted">5.000.000</small>
+                            </div>
+                            <div class="col-4">
+                                <div class="divider divider-vertical">
+                                    <div class="divider-text">
+                                        <span class="badge-divider-bg bg-label-secondary">VS</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-4 text-end">
-                            <div class="d-flex gap-2 justify-content-end align-items-center mb-2">
-                                <p class="mb-0">Pengeluaran</p>
-                                <span class="badge bg-label-primary p-1 rounded"><i class="ti ti-link ti-sm"></i></span>
+                            <div class="col-4 text-end">
+                                <div class="d-flex gap-2 justify-content-end align-items-center mb-2">
+                                    <p class="mb-0">Pengeluaran</p>
+                                    <span class="badge bg-label-primary p-1 rounded"><i class="ti ti-link ti-sm"></i></span>
+                                </div>
+                                {{-- <h5 class="mb-0 pt-1">25.5%</h5> --}}
+                                <small class="text-muted">2.000.000</small>
                             </div>
-                            {{-- <h5 class="mb-0 pt-1">25.5%</h5> --}}
-                            <small class="text-muted">2.000.000</small>
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center mt-6">
-                        <div class="progress w-100" style="height: 10px;">
-                            <div class="progress-bar bg-primary" style="width: 70%" role="progressbar" aria-valuenow="70" aria-valuemin="0"
-                                aria-valuemax="100"></div>
-                            <div class="progress-bar bg-danger" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0"
-                                aria-valuemax="100"></div>
+                        <div class="d-flex align-items-center mt-6">
+                            <div class="progress w-100" style="height: 10px;">
+                                <div class="progress-bar bg-primary" style="width: 70%" role="progressbar" aria-valuenow="70" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endhasanyrole
 
     <div class="row mt-2">
         <div class="col-12 px-0">
