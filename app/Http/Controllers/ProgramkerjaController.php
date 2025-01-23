67c0fc891a939706df875e4a8b8ef8943361c111
Adam Adifa <adamabdi.al.a@gmail.com>
2025-01-23 10:37:24 +0700

    update

--- app/Http/Controllers/ProgramkerjaController.php
+++ app/Http/Controllers/ProgramkerjaController.php
@@ -120,9 +120,10 @@ class ProgramkerjaController extends Controller
                 ->orderBy('kode_program_kerja')
                 ->first();
 
-            dd($lastprogramkerja);
+            // dd($lastprogramkerja);
             $last_kode_program_kerja = $lastprogramkerja !== null ? $lastprogramkerja->kode_program_kerja : '';
             $kode_program_kerja = buatkode($last_kode_program_kerja, $format, 4);
+            dd($kode_program_kerja);
             Programkerja::create([
                 'kode_program_kerja' => $kode_program_kerja,
                 'program_kerja' => $request->program_kerja,
