88b67a0290c7573235314dfb90d4a8ac1d1e0176
Adam Adifa <adamabdi.al.a@gmail.com>
2025-02-18 07:38:09 +0700

    update

--- resources/views/layouts/mobile/script.blade.php
+++ resources/views/layouts/mobile/script.blade.php
@@ -124,15 +124,15 @@
     $(document).ready(function() {
         function adjustZoom() {
             var width = $(window).width(); // Ambil lebar layar
-            alert(width);
-            $('body').css('zoom', '120%');
-            // if (width <= 480) { // Misalnya untuk layar kecil (mobile)
-            //     $('body').css('zoom', '80%'); // Zoom out ke 80%
-            // } else if (width <= 768) { // Untuk tablet kecil
-            //     $('body').css('zoom', '90%');
-            // } else {
-            //     $('body').css('zoom', '100%'); // Normal zoom
-            // }
+            //alert(width);
+            // $('body').css('zoom', '120%');
+            if (width <= 480) { // Misalnya untuk layar kecil (mobile)
+                $('body').css('zoom', '70%'); // Zoom out ke 80%
+            } else if (width <= 768) { // Untuk tablet kecil
+                $('body').css('zoom', '90%');
+            } else {
+                $('body').css('zoom', '100%'); // Normal zoom
+            }
         }
 
         adjustZoom(); // Panggil saat halaman dimuat
